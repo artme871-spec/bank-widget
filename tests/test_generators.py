657ee2e7257f4c8b6b9e27cf54378149f13324fb
artme871-spec <artme871@gmail.com>
2026-09-25 12:14:40 +0500

    Исправлены отступы по PEP8 для flake8

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,6 +1,7 @@
 import pytest
 from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
+
 @pytest.fixture
 def sample_transactions():
     return [
@@ -21,6 +22,7 @@ def sample_transactions():
         }
     ]
 
+
 def test_filter_by_currency(sample_transactions):
     usd_gen = filter_by_currency(sample_transactions, "USD")
     assert next(usd_gen)["id"] == 939719570
@@ -28,8 +30,10 @@ def test_filter_by_currency(sample_transactions):
     with pytest.raises(StopIteration):
         next(usd_gen)
 
+
 def test_filter_by_currency_empty():
     assert list(filter_by_currency([], "USD")) == []
+
 
 def test_transaction_descriptions(sample_transactions):
     desc_gen = transaction_descriptions(sample_transactions)
@@ -37,9 +41,11 @@ def test_transaction_descriptions(sample_transactions):
     assert next(desc_gen) == "Перевод со счета на счет"
     assert next(desc_gen) == "Перевод со счета на счет"
 
+
 @pytest.mark.parametrize("start, end, expected", [
     (1, 2, ["0000 0000 0000 0001", "0000 0000 0000 0002"]),
     (9999, 10000, ["0000 0000 0000 9999", "0000 0000 0001 0000"]),
 ])
 def test_card_number_generator(start, end, expected):
     assert list(card_number_generator(start, end)) == expected
+
